194bf23f746b7b57b02e50f90ecb3175700c2af5
samcho8 <samcho8@gmail.com>
2022-07-03 10:30:17 -0700

    added functionality to create and render posts

--- main.js
+++ main.js
@@ -1,20 +1,17 @@
 const express = require('express');
-var posts = {hello: "Lfsdfsdf"};
 const app = express();
 const PORT = 3000;
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
 app.get('/', (req, res, next) => {
-    res.render("index", posts['hello']);
+    res.render("index");
 });
 
-const createPost = (title, body) => {
-
-}
 
 const postRouter = require('./routes/posts');
 
 app.use('/posts', postRouter);
-app.listen(PORT, () => {
-    console.log(`Server listening at port ${PORT}`);
-})
+
+app.listen(PORT);
--- routes/posts.js
+++ routes/posts.js
@@ -1,29 +1,32 @@
 const express = require('express');
 const router = express.Router();
+const postBlock = {};
 
 router.get('/', (req, res) => {
-    res.send("List of posts")
+    res.render("posts/list", {jsonData: postBlock});
 })
 
 router.get('/new', (req, res) => {
-    res.send("new form");
+    res.render("posts/new");
 });
 
 router.post('/', (req, res) => {
-    res.send("creation");
+    postBlock[req.body.title] = req.body.body;
+    res.redirect('/posts');
 });
 
-router.route("/:id").get((req, res) => {
-    res.send("get specific post");
+router.route("/:title").get((req, res) => {
+   const thisPost = {};
+   thisPost[req.params.title] = postBlock[req.params.title];
+   res.render("posts/list", {jsonData: thisPost});
 }).put((req, res) => {
-    res.send("creation of a post with id");
+    res.send("creation of a post with title");
 }).delete((req, res) => {
-    res.send("deletion of post of id");
+    res.send("deletion of post of title");
 });
 
-const posts = { title: "body" };
 router.param("id", (req, res, next, id) => {
-    req.user = posts[title];
+    req.post = postBlock[title];
     next();
 });
 
