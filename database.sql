a9adc1d84c62bca295fec760c556e0c455816172
samcho8 <samcho8@gmail.com>
2022-07-12 14:54:12 -0700

    added comment system + cleaned up UI

--- database.sql
+++ database.sql
@@ -2,8 +2,24 @@ CREATE DATABASE posts;
 
 --\c into posts
 
-CREATE TABLE post ()
+CREATE TABLE post (
     post_id SERIAL PRIMARY KEY,
     title VARCHAR(255),
-    body VARCHAR(255)
+    body VARCHAR(255),
+    category_id SERIAL,
+    FOREIGN KEY(category_id) 
+        REFERENCES categories(category_id)
+);
+
+CREATE TABLE comments (
+    comment_id SERIAL PRIMARY KEY,
+    post_id SERIAL,
+    description VARCHAR(255),
+    FOREIGN KEY(post_id)
+        REFERENCES post(post_id)
+);
+
+CREATE TABLE categories (
+    category_name VARCHAR(20),
+    category_id SERIAL PRIMARY KEY
 );
--- routes/posts.js
+++ routes/posts.js
@@ -1,71 +1,82 @@
 const express = require('express');
 const pool = require('../db');
 const router = express.Router();
-const postContainer = {};
-var id = 0; // key posts by id, increment by 1 for now, but need to do differently later
 
-router.get('/', async (req, res) => {
-    const posts = await pool.query("SELECT * FROM post ORDER BY post_id asc");
-    res.render("posts/list", { jsonData: posts["rows"] });
+router.get('/:category', async (req, res) => {
+    const posts = await pool.query("SELECT * FROM post WHERE category_id = $1", 
+    [req.params.category]);
+    res.render("posts/list", { data: posts["rows"], category: req.params.category });
 })
 
-router.get('/new', (req, res) => {
-    res.render("posts/new");
+router.get('/new/:category', (req, res) => {
+    res.render("posts/new", {category: req.params.category});
 });
 
-router.post('/', async (req, res) => {
+router.post('/comment/:id/:category', async (req, res) => {
+    const id = req.params.id;
+    const description = req.body.comment;
+    const comment = await pool.query('INSERT INTO comments (post_id, description) VALUES($1, $2)',
+    [id, description]);
+    res.redirect(`/posts/${req.params.category}`);
+});
+
+router.post('/:category', async (req, res) => {
     if (!req.body.title) {
         res.status(500).send("cannot create post without a title");
         return;
     }
     const title = req.body.title;
     const body = req.body.body;
-    const newPost = await pool.query("INSERT INTO post (title, body) VALUES ($1, $2) RETURNING *",
-        [title, body]
+    const category = req.params.category;
+    const newPost = await pool.query("INSERT INTO post (title, body, category_id) VALUES ($1, $2, $3) RETURNING *",
+        [title, body, category]
     );
-    /*postContainer[id] = {};
-    postContainer[id][req.body.title] = req.body.body; 
-    id += 1;*/
-    res.redirect('/posts');
+    res.redirect(`/posts/${category}`);
 });
 
-router.post('/delete/:id', async (req, res) => {
+router.post('/delete/:id/:category', async (req, res) => {
     const { id } = req.params;
-    const deleted = await pool.query("DELETE FROM post WHERE post_id = $1",
+    const deleted_comments = await pool.query("DELETE FROM comments WHERE post_id = $1",
     [id]);
-    res.redirect('/posts');
+    const deleted_post = await pool.query("DELETE FROM post WHERE post_id = $1",
+    [id]);
+    res.redirect(`/posts/${req.params.category}`);
 });
+
 
 router.get('/edit/:id', async (req, res) => {
     const post = await pool.query("SELECT * FROM post WHERE post_id = $1",
     [req.params.id]);
-    res.render("posts/edit", { data: post["rows"][0] });
+    res.render('posts/edit', { data: post["rows"][0], category: post["rows"][0]["category_id"] });
 });
 
+router.get('/comment/:id', async (req, res) => {
+    const post = await pool.query("SELECT * FROM post WHERE post_id = $1",
+    [req.params.id]);
+    const comments = await pool.query("SELECT * FROM comments WHERE post_id = $1",
+    [req.params.id]);
+    res.render('posts/comment', {data: post["rows"][0], comments: comments["rows"], category: post["rows"][0]["category_id"]});
+});
 
-router.post('/edit/:id', (req, res) => {
+router.post('/edit/:id/:category', async (req, res) => {
     if (!req.body.title) {
-        delete postContainer[req.params.id];
         res.redirect('/posts');
         return;
     }
+    console.log(req.params);
     const id = req.params.id;
     const title = req.body.title;
     const body = req.body.body;
-    const updatedPost = pool.query("UPDATE post SET title = $1, body = $2 WHERE post_id = $3",
+    const updatedPost = await pool.query('UPDATE post SET title = $1, body = $2 WHERE post_id = $3;',
     [title, body, id]);
-    res.redirect('/posts');
-});
-router.get("/:id", (req, res) => {
-    const post = pool.query("SELECT * FROM post WHERE post_id == $1",
-    [req.params.id]
-    );
-    res.render("posts/list", { jsonData: post});
+    res.redirect(`/posts/${category}`);
 });
 
-router.param("id", (req, res, next, id) => {
-    req.post = postContainer[id];
-    next();
+router.get("/:title", async (req, res) => {
+    const post = await pool.query('SELECT * FROM post WHERE title = $1',
+    [req.params.title]
+    );
+    res.render('posts/list', { jsonData: post});
 });
 
 module.exports = router;
