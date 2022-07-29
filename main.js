e51c50a8683a808389896e22ab4ae65b23db1106
samcho8 <samcho8@gmail.com>
2022-07-28 18:03:12 -0700

    Configured main

--- main.js
+++ main.js
@@ -2,11 +2,17 @@ const express = require('express');
 const session = require('express-session');
 const dotenv = require('dotenv').config();
 const store = new session.MemoryStore();
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "views")));
+}
+
 app.use(session({
     secret: 'cookie_secret',
     cookie: { maxAge: 300000 },
